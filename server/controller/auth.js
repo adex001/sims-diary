fc77f29d04282b2efd0d770e527f3782b7fa57aa
Emmanuel Ahaiwe <emmanuelahaiwe@Emmanuels-MacBook-Pro-2.local>
2019-05-14 21:18:10 +0100

    feature(user): login user should be able to login [start #1]

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,6 +1,8 @@
 import { Op } from "sequelize";
 import models from "../models";
 import TokenHandler from "../utilities/tokenhandler";
+import verifyPassword from "../utilities/verifyPassword";
+import getUserObject from '../utilities/getUserObject';
 
 class AuthController {
   static async signup(req, res) {
@@ -36,5 +38,27 @@ class AuthController {
       });
     });
   }
+
+  static login(req, res) {
+    const { email, password } = req.body;
+
+    return models.Users.findOne( { where: { email } }).then(async (tryLogUser) => {
+      if (!tryLogUser) return res.status(400)
+      .json({ error: 'Invalid Login Details' });
+
+      const isPasswordValid = await verifyPassword(password, tryLogUser.password);
+
+      if (!isPasswordValid) return res.status(400)
+      .json({ error: 'Invalid Login Details' });
+
+      const token = await TokenHandler.createToken({
+        userId: tryLogUser.id
+      });
+
+      const user = getUserObject(tryLogUser.get());
+      return res.status(200).json({ user, token });
+    });
+    }
 }
+
 export default AuthController;
