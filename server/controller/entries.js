0147caf55f745634ec31eff43b7fc4ba54863ac7
Emmanuel Okwara <okwarae.n@gmail.com>
2019-05-15 18:20:31 +0100

    Feature(Create Diary): Create new diary
    When user wants to create a new diary, He/She enters the story/content and and posts to the endpoint (POST /entries)
    Make sure that the user has entered content to save in the diary
    [Delivers #166037024]

--- server/controller/entries.js
+++ server/controller/entries.js
@@ -1,36 +1,63 @@
 import models from "../models";
-import response from '../utilities/response';
-import entryValidator from '../validations/entries';
+import response from "../utilities/response";
+import entryValidator from "../validations/entries";
 
 class EntriesController {
-  static async updateDiary(req, res) {
-    let {
-      story
-    } = req.body;
-    const {
-      entryId
-    } = req.params;
+  static async createDiary(req, res) {
+    let { story } = req.body;
+
+    const ownerid = req.decoded.userId;
 
     const error = entryValidator.createEntry(req.body);
     if (error) return response.errorResponse(res, 400, error);
 
-    return models.Entries.findByPk(entryId)
-      .then(entry => {
-        // Check if the person that is trying to update the diary is the owner of the diary
+    try {
+      const createEntry = await models.Entries.create({
+        ownerid,
+        story
+      });
+      if (createEntry) {
+        return response.successResponse(res, 201, createEntry);
+      }
+    } catch (error) {
+      return response.errorResponse(res, 400, error);
+    }
+  }
+
+  static async updateDiary(req, res) {
+    let { story } = req.body;
+    const { entryId } = req.params;
+    req.body.entryId = entryId;
+
+    const error = entryValidator.createEntry(req.body);
+    if (error) return response.errorResponse(res, 400, error);
+
+    try {
+      const entry = await models.Entries.findByPk(entryId);
+      if (entry) {
         if (entry.dataValues.ownerid != req.decoded.userId) {
-          return response.errorResponse(res, 401, "You do not have permission to update this diary");
+          return response.errorResponse(
+            res,
+            401,
+            "You do not have permission to update this diary"
+          );
         } else {
-          entry
-            .update({
-              story
-            })
-            .then(updatedEntry => {
-              return response.successResponse(res, 200, updatedEntry);
-            })
-            .catch(error => response.errorResponse(res, 400, error));
+          const updatedEntry = await entry.update({
+            story
+          });
+
+          if (updatedEntry) {
+            return response.successResponse(res, 200, updatedEntry);
+          } else {
+            return response.errorResponse(res, 400, error);
+          }
         }
-      })
-      .catch(() => response.errorResponse(res, 404, 'Diary does not exist'));
+      } else {
+        return response.errorResponse(res, 404, "Diary does not exist");
+      }
+    } catch (error) {
+      return response.errorResponse(res, 500, error);
+    }
   }
 }
 export default EntriesController;
--- server/validations/entries.js
+++ server/validations/entries.js
@@ -1,4 +1,4 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 export default {
   createEntry(details) {
@@ -6,13 +6,11 @@ export default {
     const schema = {
       story: Joi.string()
         .required()
-        .label('Please enter a content to save')
+        .label("Please enter a content to save")
     };
 
-    const {
-      error
-    } = Joi.validate(details, schema);
+    const { error } = Joi.validate(details, schema);
     if (error) return error.details[0].context.label;
     return null;
   }
-}
+};
