2af3305959e529e08c99123322cb564140df7315
Emmanuel Okwara <okwarae.n@gmail.com>
2019-05-15 18:20:31 +0100

    Feature(Create Diary): Create new diary
    When user wants to create a new diary, He/She enters the story/content and and posts to the endpoint (POST /entries)
    Make sure that the user has entered content to save in the diary
    [Delivers #166037024]

--- server/controller/entries.js
+++ server/controller/entries.js
@@ -3,6 +3,29 @@ import response from '../utilities/response';
 import entryValidator from '../validations/entries';
 
 class EntriesController {
+  static async createDiary(req, res) {
+    let {
+      story
+    } = req.body;
+
+    const ownerid = req.decoded.userId;
+
+    const error = entryValidator.createEntry(req.body);
+    if (error) return response.errorResponse(res, 400, error);
+
+    try {
+      const createEntry = await models.Entries.create({
+        ownerid,
+        story
+      });
+      if (createEntry) {
+        return response.successResponse(res, 201, createEntry);
+      }
+    } catch (error) {
+      return response.errorResponse(res, 400, error);
+    }
+  }
+
   static async updateDiary(req, res) {
     let {
       story
@@ -14,23 +37,24 @@ class EntriesController {
     const error = entryValidator.createEntry(req.body);
     if (error) return response.errorResponse(res, 400, error);
 
-    return models.Entries.findByPk(entryId)
-      .then(entry => {
-        // Check if the person that is trying to update the diary is the owner of the diary
-        if (entry.dataValues.ownerid != req.decoded.userId) {
-          return response.errorResponse(res, 401, "You do not have permission to update this diary");
+    const entry = await models.Entries.findByPk(entryId);
+    if (entry) {
+      if (entry.dataValues.ownerid != req.decoded.userId) {
+        return response.errorResponse(res, 401, "You do not have permission to update this diary");
+      } else {
+        const updatedEntry = await entry.update({
+          story
+        });
+
+        if (updatedEntry) {
+          return response.successResponse(res, 200, updatedEntry);
         } else {
-          entry
-            .update({
-              story
-            })
-            .then(updatedEntry => {
-              return response.successResponse(res, 200, updatedEntry);
-            })
-            .catch(error => response.errorResponse(res, 400, error));
+          return response.errorResponse(res, 400, error);
         }
-      })
-      .catch(() => response.errorResponse(res, 404, 'Diary does not exist'));
+      }
+    } else {
+      return response.errorResponse(res, 404, 'Diary does not exist');
+    }
   }
 }
 export default EntriesController;
